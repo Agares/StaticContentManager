635f5e563f45550d3bf230b33a1448753f3a8698
Agares <ja@agares.info>
2014-05-21 20:25:31 +0200

    poprawki w interfejsach

--- classes/Template/ITemplate.php
+++ classes/Template/ITemplate.php
@@ -3,17 +3,17 @@ namespace Template;
 
 interface ITemplate {
 	public function getName();
-	public function setName();
+	public function setName($name);
 	
 	public function getTitle();
-	public function setTitle();
+	public function setTitle($title);
 	
 	public function getContent();
-	public function setContent();
+	public function setContent($content);
 	
 	public function getRendererName();
-	public function setRendererName();
+	public function setRendererName($rendererName);
 	
 	public function getVersion();
-	public function setVersion();
+	public function setVersion($version);
 }
--- classes/Template/ITemplateStore.php
+++ classes/Template/ITemplateStore.php
@@ -3,6 +3,9 @@
 namespace Template;
 
 interface ITemplateStore {
+	const VERSION_NEWEST = -1;
+	
 	public function addTemplateVersion(ITemplate $template);
-	public function removeTemplate($name);
+	public function removeTemplate($name);	
+	public function getTemplate($name, $version = VERSION_NEWEST);
 }
